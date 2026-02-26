b5943b97d655dc27d0068811bca84bc3482c6cf5
akanshach343-ui <akanshach343@gmail.com>
2026-02-26 11:34:30 +0530

    Create practice.java

---
+++ ADV_JAVA/practice.java
@@ -0,0 +1,30 @@
+// interface Printer{
+//   default void print(String msg){
+//     System.out.println(msg);
+//     show(msg);
+//   }
+//   private void show(String msg){
+//     System.out.println(msg.toUpperCase);
+//   }
+// }
+// public class pract implements Printer{
+//   public static void main(String[] args){
+//     Printer p= new pract();
+//     p.print("java");
+//     Printer p2= new Printer(): custom;
+//       p2.printer("lamda");
+//     }
+//   void custom(String msg){
+//     System.out.println("custom"+ msg);
+//   }
+  
+// }
+
+public class pract{
+  public static void main(String[] args){
+    var v=10;
+    Runnable r=()->{
+      System.out.println("hello");
+    }
+  }
+}
